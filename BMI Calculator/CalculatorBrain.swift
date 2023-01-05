f7fea1da23ed46d6aa59a466e4e3721263b1fadf
Egito Filho <104474051+DevEgF@users.noreply.github.com>
2023-01-04 21:25:21 -0300

    Finish project

---
+++ BMI Calculator/CalculatorBrain.swift
@@ -0,0 +1,40 @@
+//
+//  CalculateBrain.swift
+//  BMI Calculator
+//
+//  Created by EgitoMv on 1/3/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct CalculatorBrain {
+    
+    var bmi: BMI?
+    
+    mutating func calculateBMI(height: Float, weight: Float){
+        let bmiValue = weight / (height * height)
+        
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: UIColor.blue)
+        } else if bmiValue < 24.9 {
+            bmi = BMI(value: bmiValue, advice: "Fir as a fiddle!", color: UIColor.green)
+        } else {
+            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: UIColor.red)
+        }
+    }
+    
+    func getBMIValue() -> String{
+        let bmiToOneDecimalPlate = String(format: "%.1f", bmi?.value ?? 0.0)
+        return bmiToOneDecimalPlate
+    }
+    
+    func getAdvice() -> String {
+        return bmi?.advice ?? "No advice"
+    }
+    
+    func getColor() -> UIColor {
+        return bmi?.color ?? UIColor.white
+    }
+}
